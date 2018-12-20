b191dea840a4d96797ef8a0254cd99bc53c74d9e
TranDuc3997 <vanduc3997@gmail.com>
2018-12-20 16:37:04 +0700

    update Image

--- UploadMultipleImagesEMD/UploadMultipleImagesEMD/src/DTO/Image.java
+++ UploadMultipleImagesEMD/UploadMultipleImagesEMD/src/DTO/Image.java
@@ -1,25 +1,21 @@
 package DTO;
 
 public class Image {
-	public int idImage ;
-
-	public String urlImage ;
-	public int getIdImage() {
+	public int idImage;
+	public String urlImage;
+	public int getIdImage(){
 		return idImage;
 	}
-	public void setIdImage(int idImage) {
+	public void setIdImage(int idImage){
 		this.idImage = idImage;
 	}
-	
-	
-	public String getUrlImage() {
+	public String getUrlImage(){
 		return urlImage;
 	}
-	public void setUrlImage(String url) {
+	public void setUrlImage(String url){
 		this.urlImage = url;
 	}
-	public Image() {
-		super();
-		
+	public Image(){
+		super();		
 	}
 }
